6dccbdae387beef67ac7f0e76f490b8e80e28079
Yasel Gonzalez <lesaywd@gmail.com>
2022-04-26 22:59:16 -0400

    corrigiendo errores de tarea 1 que afectaban la tarea 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<mat-toolbar color="primary">
-                    <span>Material</span>
-                    <router-outlet></router-outlet>
-             </mat-toolbar> `,
+  templateUrl:'./app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
