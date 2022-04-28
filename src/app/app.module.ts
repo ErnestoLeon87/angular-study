52781f34a38c3b391b2e70f368e74c391a48374d
eleonbello@liberty.edu <2Cronicas714>
2022-04-27 21:32:47 -0400

    formatear el codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,21 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { NavegacionComponent } from './navegacion/navegacion.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
+import { NavegacionComponent } from "./navegacion/navegacion.component";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavegacionComponent
-    
-  ],
+  declarations: [AppComponent, NavegacionComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
