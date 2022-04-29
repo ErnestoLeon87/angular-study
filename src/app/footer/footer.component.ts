5a8455c839a4fda3d8a4448551ad6c2a4b95101c
Yasel Gonzalez <lesaywd@gmail.com>
2022-04-28 23:09:19 -0400

    corrigiendo errores en el footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
-  template:`<div class="footer">
-    <p>
-       <mat-toolbar color="primary">
-                   <span class="example-spacer"></span>
-                   <samp class="smp">Total de Tareas: 4</samp>
-        </mat-toolbar> 
-       </p>
-  </div>`,
+  templateUrl:'./footer.component.html',
   styleUrls: ['./footer.component.scss']
  
 })
--- src/app/navegacion/navegacion.component.ts
+++ src/app/navegacion/navegacion.component.ts
@@ -2,14 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navegacion',
-  template:`<p>
-       <mat-toolbar color="primary">
-                   <span  class="smp">Gestor de Tareas</span>
-                   <span class="example-spacer"></span>
-                   <div class="btn"><samp><button>Crear Tablas</button></samp></div>
-                   <div class="btn"><samp><button>Lista de Tareas</button></samp></div>
-          </mat-toolbar> 
-       </p>`,
+  templateUrl:'./navegacion.component.html',
   styleUrls: ['./navegacion.component.scss']
  
 })
