c039bd497f4fd418f7e3649c2625aacc643699b4
Yasel Gonzalez <lesaywd@gmail.com>
2022-04-30 23:01:09 -0400

    creando el behaviorSubject

--- src/app/tarea/tarea-service/tarea.service.service.ts
+++ src/app/tarea/tarea-service/tarea.service.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { Tarea } from '../tarea-interface/tarea.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareaServiceService {
+ 
+  private tareaBehSub=new BehaviorSubject<Tarea[]>([]);
 
   constructor() { }
+  
+  getTarea$():Observable<Tarea[]>{
+    return this.tareaBehSub.asObservable();
+  }
 }
