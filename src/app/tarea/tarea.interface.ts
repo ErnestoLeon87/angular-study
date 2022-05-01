7232e6da33482c4e2b39a773ad781ef07111f69d
Yasel Gonzalez <lesaywd@gmail.com>
2022-04-30 21:16:25 -0400

    separando enum

--- src/app/tarea/tarea.interface.ts
+++ src/app/tarea/tarea.interface.ts
@@ -1,11 +1,8 @@
+import { TareaStatus } from "./tarea-status.enum";
+
 export interface Tarea {
     id: number;
     titulo: string;
     description: string;
     status: TareaStatus;
-}
-enum TareaStatus {
-    PEMDIENTE,
-    EN_PROCESO,
-    TERMINADA
 }
