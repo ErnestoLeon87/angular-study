1bae7fc031a93a34e312d03713f7eb8b29726238
eleonbello@liberty.edu <2Cronicas714>
2022-05-02 00:52:12 -0400

    add place holders for post put and delete http methods

--- server/server.ts
+++ server/server.ts
@@ -10,6 +10,12 @@ app.use(bodyParser.json());
 
 app.route('/api/tareas').get(getAllTareas);
 
+app.route('/api/tarea').post();
+
+app.route('/api/tarea:tareaId').put();
+
+app.route('/api/tarea:tareaId').delete();
+
 const httpServer: any = app.listen(9000, () => {
   console.log(
     'HTTP REST API Server running at http://localhost:' +
