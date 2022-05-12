2690c2dcbd274d3db02806393c00dfdf14e05e4d
Yasel Gonzalez <lesaywd@gmail.com>
2022-05-12 10:26:26 -0400

    get-tareas-observer

--- src/app/core/tareas.service.ts
+++ src/app/core/tareas.service.ts
@@ -1,17 +1,24 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Tarea } from './tarea.interface';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareaService {
- 
-  private tareaBehSub=new BehaviorSubject<any>(null);
+  urlTareas='http://localhost:9000/api/tareas';
+  private tareaBehSub = new BehaviorSubject<any>(null);
+  tarea$!: Observable<Tarea>;
 
-  constructor() { }
-  
-  getTarea$():Observable<Tarea[]>{
-    return this.tareaBehSub.asObservable();
+  constructor(private http: HttpClient) {
+    this.tarea$ = this.tareaBehSub.asObservable();
+  }
+
+  getTarea$(): Observable<Tarea[]> {
+    return this.http.get<Tarea[]>(this.urlTareas);
+  }
+  private act_tareaBehSub():void{
+    this.tareaBehSub.next(this.getTarea$);
   }
 }
