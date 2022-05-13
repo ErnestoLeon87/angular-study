55d950500b7b542d71bd5e6ccc210774d6cb6446
eleonbello@liberty.edu <2Cronicas714>
2022-05-12 22:44:05 -0400

    Actualizar tareaBehSub cuando llamamos getTareas

--- src/app/core/tareas.service.ts
+++ src/app/core/tareas.service.ts
@@ -1,22 +1,24 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, map, Observable, tap } from 'rxjs';
 import { Tarea } from './tarea.interface';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TareaService {
-  urlTareas = 'http://localhost:9000/api/tareas';
+  public tareas$: Observable<Tarea[]>;
+  private url = 'http://localhost:9000/api';
   private tareaBehSub = new BehaviorSubject<Tarea[]>([]);
-  tareas$!: Observable<Tarea[]>;
 
   constructor(private http: HttpClient) {
     this.tareas$ = this.tareaBehSub.asObservable();
   }
 
   getTareas$(): Observable<Tarea[]> {
-    return this.http.get<Tarea[]>(this.urlTareas);
+    const urlGetTareas = this.url + '/tareas';
+    return this.http
+      .get<Tarea[]>(urlGetTareas)
+      .pipe(tap((tareas: Tarea[]) => this.tareaBehSub.next(tareas)));
   }
- 
 }
