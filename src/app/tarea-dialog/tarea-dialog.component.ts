9cd98f31e1d0737e0d388456a331afcc4cb65d8a
Yasel Gonzalez <lesaywd@gmail.com>
2022-07-16 11:34:23 -0400

    retirando tap y takeuntil

--- src/app/tarea-dialog/tarea-dialog.component.ts
+++ src/app/tarea-dialog/tarea-dialog.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { TareaService } from '../core/tareas.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { SpinnerService } from '../core/spinner-service/spinner.service';
-import { finalize, Subject, takeUntil, tap } from 'rxjs';
+import { finalize } from 'rxjs';
 
 
 @Component({
@@ -12,10 +12,9 @@ import { finalize, Subject, takeUntil, tap } from 'rxjs';
   styleUrls: ['./tarea-dialog.component.scss']
 })
 
-export class TareaDialogComponent implements OnInit, OnDestroy {
+export class TareaDialogComponent implements OnInit {
 
   public formTarea!: FormGroup;
-  onDestroy$: Subject<boolean> = new Subject();
 
   constructor(private formBuild: FormBuilder,
     private tareaSvc: TareaService,
@@ -30,23 +29,15 @@ export class TareaDialogComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnDestroy(): void {
-    this.onDestroy$.next(true);
-  }
-
   send(): void {
     this.spinner.activeSpinner();
     this.tareaSvc.addTarea$(this.formTarea.value).pipe(
-      tap(
-        {
-          next: dat => this._snackBar.open(dat.titulo, "se a añadido...", { duration: 2000 }),
-          error: err => this._snackBar.open(err, "", { duration: 2000 }),
-          complete: () => this.formTarea.reset({ titulo: '', description: '' })
-        }
-      ),
-      takeUntil(this.onDestroy$),
       finalize(() => this.spinner.desactiveSpinner())
-    ).subscribe();
-  }
+    ).subscribe({
+      next: dat => this._snackBar.open(dat.titulo, "se a añadido...", { duration: 2000 }),
+      error: err => this._snackBar.open(err, "", { duration: 2000 }),
+      complete: () => this.formTarea.reset({ titulo: '', description: '' })
 
+    });
+  }
 }
