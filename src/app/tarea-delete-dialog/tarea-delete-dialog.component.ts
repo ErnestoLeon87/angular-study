ab94ba00efcf0227e2e9b89e572026fbd0af0114
Yasel Gonzalez <lesaywd@gmail.com>
2022-08-17 13:43:50 -0400

    usando after close

--- src/app/tarea-delete-dialog/tarea-delete-dialog.component.ts
+++ src/app/tarea-delete-dialog/tarea-delete-dialog.component.ts
@@ -1,9 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Tarea } from '../core/tarea.interface';
 import { TareaService } from '../core/tareas.service';
-
+//renombra este componente a DeleteConfirmationDialog
 @Component({
   selector: 'app-tarea-delete-dialog',
   templateUrl: './tarea-delete-dialog.component.html',
@@ -11,19 +11,15 @@ import { TareaService } from '../core/tareas.service';
 })
 export class TareaDeleteDialogComponent implements OnInit {
 
-  constructor(private tareaSvc: TareaService,
-    private _snackBar: MatSnackBar,
+  constructor(private _snackBar: MatSnackBar,
+    public dialogRef:MatDialogRef<TareaDeleteDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public tareaDelete: Tarea
     ) { }
 
   ngOnInit(): void { }
 
-  deleteTarea(idTarea:number): void {
-   this.tareaSvc.deleteTarea(idTarea).subscribe({
-      next: dat=> this._snackBar.open('Task '+dat.titulo+' deleted successfully.', '', { duration: 2000 }),
-      error: err => this._snackBar.open(err, "", { duration: 2000 })
-    });
-
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 
 }
--- src/app/tarea/tarea.component.ts
+++ src/app/tarea/tarea.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { tap } from 'rxjs';
 import { Tarea } from '../core/tarea.interface'
+import { TareaService } from '../core/tareas.service';
 import { TareaDeleteDialogComponent } from '../tarea-delete-dialog/tarea-delete-dialog.component';
 import { TareaDialogComponent } from '../tarea-dialog/tarea-dialog.component';
 
@@ -12,16 +15,35 @@ import { TareaDialogComponent } from '../tarea-dialog/tarea-dialog.component';
 export class TareaComponent implements OnInit {
   @Input()
   tarea!: Tarea;
+  dialogRef!:MatDialogRef<TareaDeleteDialogComponent>;
 
-  constructor(public dialog: MatDialog) { }
+  constructor(public dialog: MatDialog, private tareaSvc:TareaService,private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void { }
 
   editTarea(tareaEdit: Tarea): void {
     this.dialog.open(TareaDialogComponent, { data: tareaEdit });
   }
-
+ 
   deleteTarea(TareaDelete:Tarea):void{
-    this.dialog.open(TareaDeleteDialogComponent,{ data:TareaDelete });
+   const dialogRef=this.dialog.open(TareaDeleteDialogComponent,{ data:TareaDelete });
+
+   dialogRef.afterClosed().pipe(
+    tap(result=>{
+      if(result!=undefined)
+         this.deleteTareaSvc(result.id);
+    })
+   )
+   .subscribe();
   }
+
+  deleteTareaSvc(idTarea:number): void {
+    this.tareaSvc.deleteTarea(idTarea).subscribe({
+       next: dat=> this._snackBar.open('Task '+dat.titulo+' deleted successfully.', '', { duration: 2000 }),
+       error: err => this._snackBar.open(err, "", { duration: 2000 })
+     });
+
+    }
+  
+
 }
